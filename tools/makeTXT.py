587a61f5e6ff02508d6af6c6bd08b752fedd3b14
namelesshhh <38319362+namelesshhh@users.noreply.github.com>
2021-09-23 23:13:33 +0800

    tools

---
+++ tools/makeTXT.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+import os
+dir = 'G:\Study\Data\单字\黄庭坚\金刚经R\crops'
+f = open(os.path.join(dir, 'location.txt'), 'w+')
+for curDir, dirs, files in os.walk(os.path.join(dir, '0')):
+    print("====================")
+    print("现在的目录：" + curDir)
+    print("该目录下包含的子目录：" + str(dirs))
+    print("该目录下包含的文件数目：" + str(len(files)))
+    for name in files:
+        file_name = os.path.join(curDir, name)
+        #print(file_name)
+        f.write(file_name)
+        f.write('\n')
+
+f.close()
