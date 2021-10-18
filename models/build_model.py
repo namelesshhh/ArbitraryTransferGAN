621c473b0a5ecbddeb92d62566defc975b2b1a94
amax_server_24 <2273506711@qq.com>
2021-10-18 21:18:13 +0800

    add content from server

--- models/build_model.py
+++ models/build_model.py
@@ -3,7 +3,7 @@ from .swin_transformer import SwinTransformer
 from .swin_transformer_unet_skip_expand_decoder_sys import SwinTransformerSys
 import torch.nn as nn
 class SwinUnet(nn.Module):
-    def __init__(self, config, img_size=224, num_classes=21843, zero_head=False, vis=False):
+    def __init__(self, config, img_size=224, num_classes=3, zero_head=False, vis=False):
         super(SwinUnet, self).__init__()
         self.num_classes = num_classes
         self.zero_head = zero_head
